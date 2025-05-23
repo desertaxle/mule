d761b0f5e3a596c49621bfed366a6a01e28ce4c6
Alex Streed <alex.s@prefect.io>
2025-05-22 21:30:45 -0500

    Expand the Until protocol to allow for more complex stopping conditions.
    This allows for more complex stopping conditions, such as:
    
    - Stopping if no exception is raised
    - Stopping if the exception matches a given type
    - Combining multiple Until instances with `&` and `|`

--- mule/_until.py
+++ mule/_until.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
-from typing import Protocol
+import abc
 
 from mule._attempts import AttemptContext
 
 
-class Until(Protocol):
+class Until(abc.ABC):
     """
     A protocol that defines a stopping condition for an attempting generator.
     """
 
+    @abc.abstractmethod
     def is_condition_met(self, context: AttemptContext | None) -> bool:
         """
         Checks if execution should stop.
@@ -22,10 +23,41 @@ class Until(Protocol):
         """
         ...  # pragma: no cover
 
+    def __and__(self, other: Until) -> Until:
+        return CompositeUntilAll(self, other)
+
+    def __or__(self, other: Until) -> Until:
+        return CompositeUntilAny(self, other)
+
+
+class NoException(Until):
+    """
+    An Until implementation that stops if no exception is raised in the attempt context.
+    """
+
+    def is_condition_met(self, context: AttemptContext | None) -> bool:
+        if context is None:
+            return False
+        return context.exception is None
+
+
+class ExceptionMatches(Until):
+    """
+    An Until implementation that stops if the exception matches the given type.
+    """
+
+    def __init__(self, exception_type: type[BaseException]):
+        self.exception_type = exception_type
+
+    def is_condition_met(self, context: AttemptContext | None) -> bool:
+        if context is None:
+            return False
+        return isinstance(context.exception, self.exception_type)
+
 
 class AttemptsExhausted(Until):
     """
-    A Until implementation that stops after a fixed number of attempts.
+    An Until implementation that stops after a fixed number of attempts.
 
     Attributes:
         max_attempts: The maximum number of attempts. Execution will stop after
@@ -40,6 +72,36 @@ class AttemptsExhausted(Until):
     def is_condition_met(self, context: AttemptContext | None) -> bool:
         if context is None:
             return False
-        if context and context.exception is None:
-            return True
         return context.attempt >= self.max_attempts
+
+
+class CompositeUntilAll(Until):
+    """
+    An Until implementation that stops if all of the given Until instances are met.
+    """
+
+    def __init__(self, *until: Until):
+        self.until: tuple[Until, ...] = until
+
+    def is_condition_met(self, context: AttemptContext | None) -> bool:
+        return all(until.is_condition_met(context) for until in self.until)
+
+
+class CompositeUntilAny(Until):
+    """
+    An Until implementation that stops if any of the given Until instances are met.
+    """
+
+    def __init__(self, *until: Until):
+        self.until: tuple[Until, ...] = until
+
+    def is_condition_met(self, context: AttemptContext | None) -> bool:
+        return any(until.is_condition_met(context) for until in self.until)
+
+
+def attempts_exhausted(max_attempts: int) -> Until:
+    """
+    A convenience function that returns an Until implementation keeps attempting if there is an exception,
+    but stops after a fixed number of attempts.
+    """
+    return AttemptsExhausted(max_attempts) | NoException()
